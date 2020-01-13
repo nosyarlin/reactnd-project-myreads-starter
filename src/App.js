2d49a20197e1855a50e2a7490801cf51e57f19b2
Rayson Lim Jun Kai <rayson@jublia.com>
2020-01-13 14:20:33 +0800

    feat: Update shelves using BooksAPI
    Now even when we refresh the page, changes are
    kept.

--- src/App.js
+++ src/App.js
@@ -8,66 +8,68 @@ import * as Constants from './Constants';
 
 class BooksApp extends React.Component {
   state = {
-    booksCurrentlyReading: [],
-    booksWantToRead: [],
-    booksRead: [],
+    currentlyReading: [],
+    wantToRead: [],
+    read: [],
   }
 
   componentDidMount() {
     BooksAPI.getAll()
       .then((books) => {
-        const booksCurrentlyReading = books.filter(
+        const currentlyReading = books.filter(
           (book) => book.shelf === 'currentlyReading');
-        const booksWantToRead = books.filter(
+        const wantToRead = books.filter(
           (book) => book.shelf === 'wantToRead');
-        const booksRead = books.filter(
+        const read = books.filter(
           (book) => book.shelf === 'read');
         this.setState({
-          booksCurrentlyReading: booksCurrentlyReading,
-          booksWantToRead: booksWantToRead,
-          booksRead: booksRead,
-        });
-        console.log(booksRead);
+          currentlyReading: currentlyReading,
+          wantToRead: wantToRead,
+          read: read,
+        });        
       });
   }
 
   onSelectorChange = (prevShelf, newShelf, bookToMove) => {
     const shelfKeys = {};
     // Define key mappings
-    shelfKeys[Constants.SHELVES.CURRENT_READING] = 'booksCurrentlyReading';
-    shelfKeys[Constants.SHELVES.WANT_TO_READ] = 'booksWantToRead';
-    shelfKeys[Constants.SHELVES.READ] = 'booksRead';
+    shelfKeys[Constants.SHELVES.CURRENT_READING] = 'currentlyReading';
+    shelfKeys[Constants.SHELVES.WANT_TO_READ] = 'wantToRead';
+    shelfKeys[Constants.SHELVES.READ] = 'read';
 
-    this.setState((prevState) => {
-      const out = {};
-      // Remove from previous shelf
-      if (prevShelf !== Constants.SHELVES.NONE) {
-        const prevShelfKey = shelfKeys[prevShelf];
-        const shelfToRemove = prevState[prevShelfKey].filter(
-          (book) => (book.title !== bookToMove.title)
-        );
-        out[prevShelfKey] = shelfToRemove;
-      }
-      // Add to new shelf
-      if (newShelf !== Constants.SHELVES.NONE) {
-        const newShelfKey = shelfKeys[newShelf];
-        const shelfToAdd = prevState[newShelfKey].concat([bookToMove]);
-        out[newShelfKey] = shelfToAdd;
-      }
-      return out;
-    });
+    BooksAPI.update(bookToMove, shelfKeys[newShelf])
+      .then(
+        this.setState((prevState) => {
+          const out = {};
+          // Remove from previous shelf
+          if (prevShelf !== Constants.SHELVES.NONE) {
+            const prevShelfKey = shelfKeys[prevShelf];
+            const shelfToRemove = prevState[prevShelfKey].filter(
+              (book) => (book.title !== bookToMove.title)
+            );
+            out[prevShelfKey] = shelfToRemove;
+          }
+          // Add to new shelf
+          if (newShelf !== Constants.SHELVES.NONE) {
+            const newShelfKey = shelfKeys[newShelf];
+            const shelfToAdd = prevState[newShelfKey].concat([bookToMove]);
+            out[newShelfKey] = shelfToAdd;
+          }
+          return out;
+        })
+      );
   }
 
   render() {
-    const { booksCurrentlyReading, booksWantToRead, booksRead } = this.state;
+    const { currentlyReading, wantToRead, read } = this.state;
     return (
       <div className="app">
         <Route exact path='/' render={() => (
             <MyReadsPage
               onSelectorChange={this.onSelectorChange}
-              booksCurrentlyReading={booksCurrentlyReading}
-              booksWantToRead={booksWantToRead}
-              booksRead={booksRead}
+              currentlyReading={currentlyReading}
+              wantToRead={wantToRead}
+              read={read}
             />
           )}
         />
--- src/MyReadsPage.js
+++ src/MyReadsPage.js
@@ -5,7 +5,7 @@ import * as Constants from './Constants';
 
 class MyReadsPage extends Component {
   render() {
-    const { onSelectorChange, booksCurrentlyReading, booksWantToRead, booksRead } = this.props;
+    const { onSelectorChange, currentlyReading, wantToRead, read } = this.props;
     return (
       <div className="list-books">
         <div className="list-books-title">
@@ -15,17 +15,17 @@ class MyReadsPage extends Component {
           <div>
             <BookShelf 
               shelf={Constants.SHELVES.CURRENT_READING} 
-              books={booksCurrentlyReading}
+              books={currentlyReading}
               onSelectorChange={onSelectorChange}
             />
             <BookShelf 
               shelf={Constants.SHELVES.WANT_TO_READ} 
-              books={booksWantToRead}
+              books={wantToRead}
               onSelectorChange={onSelectorChange}
             />
             <BookShelf 
               shelf={Constants.SHELVES.READ} 
-              books={booksRead}
+              books={read}
               onSelectorChange={onSelectorChange}
             />
           </div>
